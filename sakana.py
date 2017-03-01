f712a6d68a60ebfc320e5c576cea18cf5e1e632a
autotaker <autotaker@gmail.com>
2017-03-01 13:33:59 +0000

    implement sakana

--- sakana.py
+++ sakana.py
@@ -1,23 +1,40 @@
 from api import *
 import time, os
 from websockets.exceptions import ConnectionClosed
+from capture import *
+import asyncio
 
-def sakana_capture():
-    os.system('./upload.sh {}'.format('sample.mp4'))
+capture_busy = False
 
-def sakana(cmd):
+def sakana_capture(channel):
+    global capture_busy
+    if capture_busy:
+        return 'busy'
+    async def capture_async():
+        global capture_busy
+        try:
+            capture('data/')
+            os.system('./upload.sh {} {}'.format('data/capture.mp4', channel))
+        finally:
+            capture_busy = False
+    capture_busy = True
+    asyncio.ensure_future(capture_async())
+    return 'capturing'
+    
+
+def sakana(cmd, event, rtm):
     cmd = cmd.split()
     if len(cmd) == 0:
         return None
     if cmd[0] != 'sakana':
         return None
-    return sakana_capture()
+    return sakana_capture(event["channel"])
 
 def handler(rtm, event):
     if event["type"] == "message" and 'bot_id' not in event:
         msg = event["text"]
         print("Message({})".format(repr(event["text"])))
-        ret = sakana(msg)
+        ret = sakana(msg, event, rtm)
         if ret:
             print(ret)
             return rtm.send_message_rtm(ret, 
--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
-
+#!/bin/bash
 source secrets.sh
 
-curl  -F token=$SLACK_API_TOKEN -F file=@sample.mp4 -F filename=sample.mp4 -F channels='#bottest' "https://slack.com/api/files.upload"
+curl  -F token=$SLACK_API_TOKEN -F file=@$1 \
+      -F filename=sample.mp4  \
+      -F channels="$2" "https://slack.com/api/files.upload"
 
